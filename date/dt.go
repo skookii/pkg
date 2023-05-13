c3b6f42147c828f9b04f7c45e848b365936a93a5
skookii <support@skookii.com>
2023-05-13 15:47:01 +0500

    unix time

--- date/dt.go
+++ date/dt.go
@@ -1,6 +1,9 @@
 package date
 
-import "time"
+import (
+	"strconv"
+	"time"
+)
 
 func Today() time.Time {
 	now := time.Now()
@@ -19,4 +22,19 @@ func TimeToDate(t time.Time) time.Time {
 func Tomorrow() time.Time {
 	now := time.Now().AddDate(0, 0, 1)
 	return time.Date(now.Year(), now.Month(), now.Day(), 0, 0, 0, 0, time.Local)
+}
+
+// time to unix string
+func UnixTimeString(tm time.Time) string {
+	i64 := tm.Unix()
+	return strconv.FormatInt(i64, 10)
+}
+
+// unix string to time
+func StringToTime(s string) time.Time {
+	i64, err := strconv.ParseInt(s, 10, 64)
+	if err != nil {
+		return time.Date(2010, 1, 1, 0, 0, 0, 0, time.Local)
+	}
+	return time.Unix(i64, 0)
 }
